54557f4a7d83de2657985ccab09691e95a4afc9a
evglevdorovich <lev_jack@mail.ru>
2024-05-25 23:38:12 +0200

    init

---
+++ src/main/java/com/learning/enterprisepatterns/EnterprisepatternsApplication.java
@@ -0,0 +1,13 @@
+package com.learning.enterprisepatterns;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnterprisepatternsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnterprisepatternsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/learning/enterprisepatterns/EnterprisepatternsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learning.enterprisepatterns;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnterprisepatternsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
